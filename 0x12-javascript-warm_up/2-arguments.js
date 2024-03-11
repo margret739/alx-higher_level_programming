99844e26e2e88bfc960242bbe75e17198845b927
margret739 <margretandalya@gmail.com>
2024-03-11 14:19:10 -0700

    arguments js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const myArgs = 2;
+
+if (myArgs === 0) {
+	console.log('No arguments');
+}
+if (myArgs === 1) {
+	console.log('Argument found');
+}
+else {
+	console.log('Arguments found');
+}
